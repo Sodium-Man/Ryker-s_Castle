f2f47c89093cc745ac90a95165763170d6c4ca1d
Jawahar <jawaharlal.h.kanoujia@slrtce.in>
2022-09-25 11:11:16 +0530

    map while loop

--- Ryker's_Castle/src/main/GamePanel.java
+++ Ryker's_Castle/src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
 	
@@ -15,21 +16,19 @@ public class GamePanel extends JPanel implements Runnable{
 	final int scale = 3;
 	
 	public final int tileSize = originalTileSize * scale;
-	final int maxScreenCol = 16;
-	final int maxScreenRow = 12;
-	final int screenWidth = tileSize*maxScreenCol;
-	final int screenHeight = tileSize*maxScreenRow;
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize*maxScreenCol;
+	public final int screenHeight = tileSize*maxScreenRow;
 	
 	//FPS
 	int FPS=60;
 	
+	TileManager tileM =new TileManager(this);
 	KeyHandler keyH=new KeyHandler();
 	Thread gameThread;
 	Player player=new Player(this,keyH);
 	
-	int playerX= 100;
-	int playerY= 100;
-	int playerSpeed=4;
 	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -66,11 +65,6 @@ public class GamePanel extends JPanel implements Runnable{
 				delta--;
 				drawCount++;
 			}
-			if(timer >= 1000000000) {
-				System.out.println("FPS"+drawCount);
-				drawCount=0;
-				timer=0;
-			}
 		}
 	}
 	public void update() {
@@ -83,6 +77,8 @@ public class GamePanel extends JPanel implements Runnable{
 		super.paintComponent(g);
 		
 		Graphics2D g2=(Graphics2D)g;
+		
+		tileM.draw(g2);
 		player.draw(g2);
 	
 		g2.dispose();
